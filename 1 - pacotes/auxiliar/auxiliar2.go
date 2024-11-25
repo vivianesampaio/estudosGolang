cacc0916ea3d504d0a197fc1a47d3f7ad7743264
Viviane Sampaio <107593709+vivianesampaio@users.noreply.github.com>
2024-11-25 12:26:30 -0300

    Add files via upload

---
+++ 1 - pacotes/auxiliar/auxiliar2.go
@@ -0,0 +1,7 @@
+package auxiliar
+
+import "fmt"
+
+func escrever2() {
+	fmt.Println("Escrevendo pelo pacote auxiliar em um escopo não acessível")
+}
---
+++ 1 - pacotes/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"modulo/auxiliar"
+)
+
+func main() {
+	fmt.Println("Hello Go. Escrevendo do arquivo MAIN")
+	auxiliar.Escrever()
+}
