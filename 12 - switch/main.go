20659c9779a59cfdbdec17ac99d6b9bab1d15a11
Viviane Sampaio <107593709+vivianesampaio@users.noreply.github.com>
2025-02-04 23:36:51 -0300

    Add files via upload

---
+++ 12 - switch/main.go
@@ -0,0 +1,113 @@
+package main
+
+import "fmt"
+
+func diaDaSemana(numero int) string {
+	switch numero {
+	case 1:
+		return "Domingo"
+	case 2:
+		return "Segunda-Feira"
+	case 3:
+		return "Terça-feira"
+	case 4:
+		return "Quarta-feira"
+	case 5:
+		return "Quinta-feira"
+	case 6:
+		return "Sexta-feira"
+	case 7:
+		return "Sábado"
+	default:
+		return "Número inválido"
+	}
+}
+
+func diaDaSemana2(numero int) string {
+	switch {
+	case numero == 1:
+		return "Domingo"
+	case numero == 2:
+		return "Segunda-feira"
+	case numero == 3:
+		return "Terça-feira"
+	case numero == 4:
+		return "Quarta-feira"
+	case numero == 5:
+		return "Quinta-feira"
+	case numero == 6:
+		return "Sexta-feira"
+	case numero == 7:
+		return "Sábado"
+	default:
+		return "número inválido"
+
+	}
+
+}
+
+func diaDaSemanaFallThrough(numero int) string {
+
+	var diaDaSemana string
+
+	switch {
+	case numero == 1:
+		diaDaSemana = "Domingo"
+	case numero == 2:
+		diaDaSemana = "Segunda-feira"
+	case numero == 3:
+		diaDaSemana = "Terça-feira"
+	case numero == 4:
+		diaDaSemana = "Quarta-feira"
+	case numero == 5:
+		diaDaSemana = "Quinta-feira"
+	case numero == 6:
+		diaDaSemana = "Sexta-feira"
+	case numero == 7:
+		diaDaSemana = "Sábado"
+	default:
+		diaDaSemana = "número inválido"
+	}
+	return diaDaSemana
+}
+
+func diaDaSemanaFallThrough2(numero int) string {
+
+	var diaDaSemana string
+
+	switch {
+	case numero == 1:
+		diaDaSemana = "Domingo"
+		fallthrough
+	case numero == 2:
+		diaDaSemana = "Segunda-feira"
+	case numero == 3:
+		diaDaSemana = "Terça-feira"
+	case numero == 4:
+		diaDaSemana = "Quarta-feira"
+	case numero == 5:
+		diaDaSemana = "Quinta-feira"
+	case numero == 6:
+		diaDaSemana = "Sexta-feira"
+	case numero == 7:
+		diaDaSemana = "Sábado"
+	default:
+		diaDaSemana = "número inválido"
+	}
+	return diaDaSemana
+}
+
+func main() {
+	fmt.Println("Switch Case")
+	dia := diaDaSemana(3)
+	fmt.Println(dia)
+
+	dia2 := diaDaSemana2(10)
+	fmt.Println(dia2)
+
+	dia3 := diaDaSemanaFallThrough(10)
+	fmt.Println(dia3)
+
+	dia4 := diaDaSemanaFallThrough2(3)
+	fmt.Println(dia4)
+}
